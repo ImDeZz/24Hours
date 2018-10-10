f4e6350eed00c39291f1530d754a0878fa87a08d
groli96 <roland.grager@gmail.com>
2018-10-10 11:08:42 +0200

    Created general GUI

--- 24h_audio/24h_audio/Form1.Designer.cs
+++ 24h_audio/24h_audio/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openedFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(702, 404);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(75, 23);
+            this.quitButton.TabIndex = 0;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openedFilePathTextBox
+            // 
+            this.openedFilePathTextBox.Location = new System.Drawing.Point(34, 364);
+            this.openedFilePathTextBox.Name = "openedFilePathTextBox";
+            this.openedFilePathTextBox.ReadOnly = true;
+            this.openedFilePathTextBox.Size = new System.Drawing.Size(642, 20);
+            this.openedFilePathTextBox.TabIndex = 1;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(702, 362);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(702, 309);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 3;
+            this.playButton.TabStop = false;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.openedFilePathTextBox);
+            this.Controls.Add(this.quitButton);
+            this.Name = "Form1";
+            this.Text = "Audio App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox openedFilePathTextBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button playButton;
     }
 }
 
--- 24h_audio/24h_audio/Form1.cs
+++ 24h_audio/24h_audio/Form1.cs
@@ -12,9 +12,32 @@ namespace _24h_audio
 {
     public partial class Form1 : Form
     {
+        public string selectedFile;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select File";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                openedFilePathTextBox.Text = openFileDialog1.FileName;
+                this.selectedFile = openFileDialog1.FileName;
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
